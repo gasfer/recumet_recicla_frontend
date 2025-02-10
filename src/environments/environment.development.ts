a2fcee287e19bb3490d6168948c59460bf22cc43
gasfer <gasfer.seca@gmail.com>
2025-02-10 19:51:46 -0400

    cambio de color del estado de pago

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    /*base_url: 'https://reciclapi.recumetbolivia.com/api/v1'*/
+    //base_url: 'https://reciclapi.recumetbolivia.com/api/v1'
     base_url: 'http://localhost:3000/api/v1'
 };
