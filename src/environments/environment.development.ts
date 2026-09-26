d77866535e2c8624f1069a9551ff29c0349ac7be
Gaston fernandez <gasfer.seca@gmail.com>
2026-09-26 10:32:31 -0400

    update env prod

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,4 @@
 export const environment = {
     //base_url: 'https://reciclapi.recumetbolivia.com/api/v1'
-    base_url: 'http://localhost:3000/api/v1',
-    features: {
-      transferReconciliationAlert: false,
-    },
+    base_url: 'http://localhost:3000/api/v1'
 };
